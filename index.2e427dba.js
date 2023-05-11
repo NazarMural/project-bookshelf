9260ca48e66a96994577be8ca0f9c10c5c61525b
NazarMural <61448859+NazarMural@users.noreply.github.com>
2023-05-11 21:06:48 +0000

    Deploying to gh-pages from @ NazarMural/project-bookshelf@cf3b8f1fa3042e01b0875a7ccb4f0708c1a8ed19 🚀

---
+++ index.2e427dba.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-open-menu"),n=document.querySelector(".js-menu-container");e.addEventListener("click",(function(){e.classList.toggle("is-open"),n.classList.toggle("is-open")}));
+//# sourceMappingURL=index.2e427dba.js.map
---
+++ index.8e8e5474.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-open-menu"),n=document.querySelector(".js-menu-container");e.addEventListener("click",(function(){e.classList.toggle("is-open"),n.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.8e8e5474.js.map
