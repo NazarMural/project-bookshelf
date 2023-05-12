578fb4117641a0bc096d25ee59c91757f71b611f
NazarMural <61448859+NazarMural@users.noreply.github.com>
2023-05-12 15:38:04 +0000

    Deploying to gh-pages from @ NazarMural/project-bookshelf@38d322a6773c857976eb916f13efd3fe0b7e2b7e 🚀

--- index.2e427dba.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".js-open-menu"),n=document.querySelector(".js-menu-container");e.addEventListener("click",(function(){e.classList.toggle("is-open"),n.classList.toggle("is-open")}));
-//# sourceMappingURL=index.2e427dba.js.map
