9392e7a932ae29c98f010f6ee4024edcf96f393b
TetiPalii <TetianaPalii9491@gmail.com>
2023-05-14 17:48:26 +0200

    adding buy links

--- src/js/aboutBookModal.js
+++ src/js/aboutBookModal.js
@@ -5,14 +5,18 @@ import {
   getBook,
   openSignUpFunc,
 } from './authentication';
+import iconAmazonUrl from '../images/amazon.png';
+import iconBooksUrl from '../images/books.png';
+import iconBookShopUrl from '../images/bookShop.png';
 
 const booksCardsList = document.querySelector('.books-cards__list');
-const backdrop = document.querySelector('.backdrop--hidden');
+const backdrop = document.querySelector('.backdrop');
 const btnClose = document.querySelector('.close-btn');
 const bookContainer = document.querySelector('.book-container');
 const modalBtn = document.querySelector('.modal__btn');
 const modalBookBtnSignUp = document.querySelector('.modal-book-btn__signUp');
 const underRemoveBtn = document.querySelector('.information-about-status');
+const storeIconsList = document.querySelector('.store-icons');
 
 booksCardsList.addEventListener('click', openModal);
 modalBookBtnSignUp.addEventListener('click', openSignUpFunc);
@@ -27,16 +31,14 @@ async function openModal(e) {
     e.target.nodeName === 'A'
   ) {
     bookContainer.innerHTML = '';
-    // backdrop.style.zIndex = 0;
-
     btnClose.addEventListener('click', onClosebtn);
     const bookItem = e.target.closest('.category-books__item');
     const bookId = bookItem.dataset.id;
     backdrop.classList.remove('backdrop--hidden');
 
     const bookMarkup = await fetchSearchResult(bookId);
-    console.log(bookMarkup);
-    bookContainer.insertAdjacentHTML('afterbegin', createBookMarup(bookMarkup));
+    console.log(bookMarkup.buy_links);
+    bookContainer.insertAdjacentHTML('beforeend', createBookMarup(bookMarkup));
 
     const varGetBook = await getBook();
     if (varGetBook !== null) {
@@ -106,9 +108,42 @@ function createBookMarup({
   <div class="text-container"><h2 class="book-cover__title">${title}</h2>
   <p class="book-cover__author">${author}</p>
   <p class="book-cover__description">${description}</p>
+  <ul class="store-icons">
+  <li class="store-icons__item">
+    <a href="${buy_links}"
+      ><img
+        src="${iconAmazonUrl}"
+        alt="book store"
+        width="62"
+        height="19"
+        class="store-icon"
+    /></a>
+  </li>
+  <li class="store-icons__item">
+    <a href="${buy_links}" target="_blank" rel="noreferrer noopener"
+      ><img
+        src="${iconBooksUrl}"
+        alt="book store"
+        width="33"
+        height="32"
+        class="store-icon"
+    /></a>
+  </li>
+  <li class="store-icons__item">
+    <a href="${filterlink(buy_links)}"
+      ><img
+        src="${iconBookShopUrl}"
+        alt="book store"
+        width="38"
+        height="36"
+        class="store-icon"
+    /></a>
+  </li>
+</ul>
   </div>`;
 
   modalBtn.dataset.id = `${_id}`;
+  // console.log(filterlink(buy_links));
   return markup;
 }
 
@@ -126,3 +161,9 @@ function onKeyDown({ code }) {
     backdrop.classList.add('backdrop--hidden');
   }
 }
+
+// function filterlink(arr) {
+//   arr.map(({ url }) => {
+//     return url;
+//   });
+// }
