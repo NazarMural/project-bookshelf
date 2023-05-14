4648092ab7f1f98339b8a574e494c404e962535a
moses35 <energetic143@gmail.com>
2023-05-14 21:27:27 +0300

    add theme save to local storage

--- src/js/switch.js
+++ src/js/switch.js
@@ -5,75 +5,77 @@ const logotextEl = document.querySelector('.logo__text');
 const btnBurgerOpen = document.querySelector('.js-open-menu');
 const headerLinEl = document.querySelector('.js-link-home');
 const headerLinkElS = document.querySelector('.js-link-shoping');
-const iconEl= document.querySelector('.js-icon');
-const iconBurgerOpen=document.querySelector('.icon__burger--open')
-const btnBurgerClose=document.querySelector('.js-close-menu')
-const iconBurgerClose=document.querySelector('.icon__burger--close')
+const iconEl = document.querySelector('.js-icon');
+const iconBurgerOpen = document.querySelector('.icon__burger--open');
+const btnBurgerClose = document.querySelector('.js-close-menu');
+const iconBurgerClose = document.querySelector('.icon__burger--close');
 
 const toggleEl = document.querySelector('#toggle');
+
+//read local storage
+const mainTheme = localStorage.getItem('theme');
+if (mainTheme === 'dark') {
+  onclickToggleEl();
+}
+
+//toggle
 toggleEl.addEventListener('click', onclickToggleEl);
 
 function onclickToggleEl(event) {
+  bodyEl.classList.toggle('dark-theme');
+  headerEl.classList.toggle('dark');
+  logotextEl.classList.toggle('dark__icon');
+  btnBurgerOpen.classList.toggle('button-black');
+  iconBurgerOpen.classList.toggle('icon-black');
+  btnBurgerClose.classList.toggle('button-black');
+  iconBurgerClose.classList.toggle('icon-black');
 
-    bodyEl.classList.toggle('dark-theme');
-    headerEl.classList.toggle('dark');
-    logotextEl.classList.toggle('dark__icon');
-    btnBurgerOpen.classList.toggle('button-black');
-    iconBurgerOpen.classList.toggle('icon-black')
-    btnBurgerClose.classList.toggle('button-black')
-    iconBurgerClose.classList.toggle('icon-black')
+  if (!headerLinEl.classList.contains('current')) {
+    headerLinEl.classList.toggle('dark__text');
+  }
+  if (!headerLinkElS.classList.contains('current')) {
+    headerLinkElS.classList.toggle('dark__text');
+  }
 
-    if (!headerLinEl.classList.contains('current')) {
-        headerLinEl.classList.toggle('dark__text')
-    }
-    if (!headerLinkElS.classList.contains('current')) {
-        headerLinkElS.classList.toggle('dark__text')
-    }
-   
-    if (!iconEl.classList.contains('current')) {
-        iconEl.classList.toggle('dark__icon')
-    }
-    
-    // if (bodyEl.classList.contains('dark-theme')
-    // ) {
-    //     localStorage.setItem('DARKBODY', 'dark-theme');
-    //     return;
-        
-    // }
-    // localStorage.removeItem('DARK');
-    
-    // if( headerEl.classList.contains('dark') ||
-    //     logotextEl.classList.contains('dark__icon') || btnBurgerOpen.classList.contains('button-black') 
-    //     ||  iconBurgerOpen.classList.contains('icon-black') || btnBurgerClose.classList.contains('button-black')
-    // || iconBurgerClose.classList.contains('icon-black') || headerLinEl.classList.contains('dark__text')
-    // ||  headerLinkElS.classList.contains('dark__text') ||  iconEl.classList.toggle('dark__icon')) {
-    //     // localStorage.setItem('DARKBODY', 'dark-theme');
-    //     localStorage.setItem('DARKHEADER', 'dark');
-    //     localStorage.setItem('DARKLOGOTEXT', 'dark__icon');
-    //     localStorage.setItem('DARKBTNBURGEROPEN', 'button-black');
-    //     localStorage.setItem('DARCICONOPEN', 'icon-black');
-    //     localStorage.setItem('DARKBTNBURGERCLOSE', 'button-black');
-    //     localStorage.setItem('DARCICONCLOSE', 'icon-black');
-    //     localStorage.setItem('DARKTEXT', 'dark__text');
-    //     localStorage.setItem('DARKTEXTLINK', 'dark__text');
-    //     localStorage.setItem('DARKICONEL', 'dark__icon')
-    //     return;
-        
-    // }
+  if (!iconEl.classList.contains('current')) {
+    iconEl.classList.toggle('dark__icon');
+  }
 
-    // // localStorage.removeItem('DARK');
-    // localStorage.removeItem('DARKHEADER');
-    // localStorage.removeItem('DARKLOGOTEXT');
-    // localStorage.removeItem('DARKBTNBURGEROPEN');
-    // localStorage.removeItem('DARCICONOPEN');
-    // localStorage.removeItem('DARKBTNBURGERCLOSE');
-    // localStorage.removeItem('DARCICONCLOSE');
-    // localStorage.removeItem('DARKTEXT');
-    // localStorage.removeItem('DARKTEXTLINK');
-    // localStorage.removeItem('DARKICONEL');
+  if (!bodyEl.classList.contains('dark-theme')) {
+    localStorage.setItem('theme', 'light');
+  } else {
+    localStorage.setItem('theme', 'dark');
+  }
 
-    
-   
+  // localStorage.removeItem('DARK');
+
+  // if( headerEl.classList.contains('dark') ||
+  //     logotextEl.classList.contains('dark__icon') || btnBurgerOpen.classList.contains('button-black')
+  //     ||  iconBurgerOpen.classList.contains('icon-black') || btnBurgerClose.classList.contains('button-black')
+  // || iconBurgerClose.classList.contains('icon-black') || headerLinEl.classList.contains('dark__text')
+  // ||  headerLinkElS.classList.contains('dark__text') ||  iconEl.classList.toggle('dark__icon')) {
+  //     // localStorage.setItem('DARKBODY', 'dark-theme');
+  //     localStorage.setItem('DARKHEADER', 'dark');
+  //     localStorage.setItem('DARKLOGOTEXT', 'dark__icon');
+  //     localStorage.setItem('DARKBTNBURGEROPEN', 'button-black');
+  //     localStorage.setItem('DARCICONOPEN', 'icon-black');
+  //     localStorage.setItem('DARKBTNBURGERCLOSE', 'button-black');
+  //     localStorage.setItem('DARCICONCLOSE', 'icon-black');
+  //     localStorage.setItem('DARKTEXT', 'dark__text');
+  //     localStorage.setItem('DARKTEXTLINK', 'dark__text');
+  //     localStorage.setItem('DARKICONEL', 'dark__icon')
+  //     return;
+
+  // }
+
+  // // localStorage.removeItem('DARK');
+  // localStorage.removeItem('DARKHEADER');
+  // localStorage.removeItem('DARKLOGOTEXT');
+  // localStorage.removeItem('DARKBTNBURGEROPEN');
+  // localStorage.removeItem('DARCICONOPEN');
+  // localStorage.removeItem('DARKBTNBURGERCLOSE');
+  // localStorage.removeItem('DARCICONCLOSE');
+  // localStorage.removeItem('DARKTEXT');
+  // localStorage.removeItem('DARKTEXTLINK');
+  // localStorage.removeItem('DARKICONEL');
 }
-
-
