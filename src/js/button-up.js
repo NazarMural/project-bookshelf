09b8b7c65becd88aec7de867a143ce5156f6e7a5
NazarMural <nazarmural777@gmail.com>
2023-05-15 21:39:42 +0300

    fix bag

--- src/js/button-up.js
+++ src/js/button-up.js
@@ -6,9 +6,21 @@ refs.btnToTop.addEventListener('click', () => {
   scrollUp();
 });
 
+window.addEventListener('scroll', () => {
+  toggleBtnVisibility();
+});
+
 export function scrollUp() {
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
   });
+}
+
+function toggleBtnVisibility() {
+  if (window.pageYOffset > 0) {
+    refs.btnToTop.classList.remove('is-hidden');
+  } else {
+    refs.btnToTop.classList.add('is-hidden');
+  }
 }
